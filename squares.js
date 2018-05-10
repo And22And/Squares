e1176e9abcaf2ee8afc68f8a80c96a67aa6ef26f
And22And <a181096@gmail.com>
2018-05-10 16:09:22 +0300

    table in table to table in div

--- squares.js
+++ squares.js
@@ -1,25 +1,31 @@
-var numberOfRows = 1;
-var numberOfColumns = 1;
 var currentButtonLeft = 0;
 var currentButtonTop = 0;
 var tdTemplate = "<td onmouseover='overCell(this)'></td>";
 var trTemplate = "<tr onmouseover='overRow(this)'>";
 var tdRemoveTemplate = "<td></td>";
 
+function numberOfRows(){
+    return getSquares().children.length;
+}
+
+function numberOfColumns(){
+    return getSquares().children[0].children.length;
+}
+
 function leftButton(){
-    return document.getElementsByClassName("leftButton")[0];
+    return document.getElementsByClassName("leftButton")[0].children[0];
 }
 
 function rightButton(){
-    return document.getElementsByClassName("rightButton")[0];
+    return document.getElementsByClassName("rightButton")[0].children[0];
 }
 
 function topButton(){
-    return document.getElementsByClassName("topButton")[0];
+    return document.getElementsByClassName("topButton")[0].children[0];
 }
 
 function bottomButton(){
-    return document.getElementsByClassName("bottomButton")[0];
+    return document.getElementsByClassName("bottomButton")[0].children[0];
 }
 
 function getSquares(){
@@ -29,55 +35,43 @@ function getSquares(){
 function addRow(){
     var i;
     var row = trTemplate;
-    for(i = 0; i < numberOfColumns; i++) {
+    for(i = 0; i < numberOfColumns(); i++) {
         row += tdTemplate;
     }
     row += "</tr>"
     getSquares().insertAdjacentHTML("beforeend", row);
-    rightButton().insertAdjacentHTML("beforeend", "<tr><td></td></tr>");
-    leftButton().insertAdjacentHTML("beforeend", "<tr>" + tdRemoveTemplate + "</tr>");
-    numberOfRows++;
 }
 
 function removeRow(){
-    rightButton().lastChild.remove();
-    if(numberOfRows == 2) {
-        leftButton().children[currentButtonLeft].remove();
-    } else {
-        leftButton().children[currentButtonLeft == leftButton().children.length - 1 ? currentButtonLeft - 1 : currentButtonLeft + 1].remove();
-    }
     getSquares().children[currentButtonLeft].remove();
-    if(currentButtonLeft == leftButton().children.length) {
+    if(currentButtonLeft == numberOfRows()) {
         currentButtonLeft--;
     }
-    numberOfRows--;
+    leftButton().style.marginTop = getOffsetTop((getSquares().children[currentButtonLeft])) + "px";
+    if(numberOfRows() == 1){
+        removeButton(leftButton());
+    }
 }
 
 function addColumn(){
     var rows = Array.from(getSquares().children);
     rows.forEach(function(row) {
         row.insertAdjacentHTML("beforeend", tdTemplate);
-    }); 
-    bottomButton().children[0].insertAdjacentHTML("beforeend", "<td></td>");
-    topButton().children[0].insertAdjacentHTML("beforeend", tdRemoveTemplate);
-    numberOfColumns++;    
+    });   
 }
 
 function removeColumn(){
-    bottomButton().firstChild.lastChild.remove();
-    if(numberOfColumns == 2) {
-        topButton().firstChild.children[currentButtonTop].remove();
-    } else {
-        topButton().firstChild.children[currentButtonTop == topButton().firstChild.children.length - 1 ? currentButtonTop - 1 : currentButtonTop + 1].remove();
-    }
     var rows = Array.from(getSquares().children);
     rows.forEach(function(row) {
         row.children[currentButtonTop].remove();
     });
-    if(currentButtonTop == topButton().firstChild.children.length) {
+    if(currentButtonTop == numberOfColumns()) {
         currentButtonTop--;
     }
-    numberOfColumns--;
+    topButton().style.marginLeft = getOffsetLeft((getSquares().children[currentButtonLeft].children[currentButtonTop])) + "px";
+    if(numberOfColumns() == 1){
+        removeButton(topButton());
+    }
 }
 
 function findPosition(elem){
@@ -85,52 +79,57 @@ function findPosition(elem){
 }
 
 function addButton(elem){
-    elem.innerHTML = '-';
-    elem.classList.add('button');
+    elem.style.visibility = "visible";
 }
 
 function removeButton(elem){
-    elem.innerHTML = '';
-    elem.onclick = null;
-    elem.classList.remove('button');
+    elem.style.visibility = "hidden";
 }
 
 function outCell(){  
-    if(currentButtonTop < numberOfColumns) {
-        removeButton(topButton().firstChild.children[currentButtonTop]);
+    if(currentButtonTop < numberOfColumns()) {
+        removeButton(topButton());
     }
 }
 
 function overCell(obj){
-    if(numberOfColumns != 1) { 
-        outCell();
+    if(numberOfColumns() != 1) { 
         currentButtonTop = findPosition(obj);
-        var elem = topButton().firstChild.children[currentButtonTop];
-        addButton(elem);
-        elem.onclick = removeColumn;
-    }    
+        addButton(topButton());
+        topButton().style.marginLeft = getOffsetLeft((getSquares().children[currentButtonLeft].children[currentButtonTop])) + "px";
+    }
 }
 
 function outRow(){
-    if(currentButtonLeft < numberOfRows) {        
-        //alert("But = " + leftButton() + ", len = " + leftButton().children.length + ", cur = " + currentButtonLeft + ", child = " + leftButton().children[currentButtonLeft] + ", elem = " + leftButton().children[currentButtonLeft].firstChild);
-        removeButton(leftButton().children[currentButtonLeft].firstChild);
+    if(currentButtonLeft < numberOfRows()) {        
+        removeButton(leftButton());
     }    
 }
 
 function overRow(obj){
-    if(numberOfRows != 1) { 
-        outRow();
+    if(numberOfRows() != 1) { 
         currentButtonLeft = findPosition(obj);
-        var elem = leftButton().getElementsByTagName('td')[currentButtonLeft];
-        addButton(elem);
-        elem.onclick = removeRow;
-    }   
+        addButton(leftButton());
+        leftButton().style.marginTop = getOffsetTop((getSquares().children[currentButtonLeft])) + "px";
+    }
+}
+
+function getOffsetLeft(elem){
+    return elem.getBoundingClientRect().left - document.getElementById("squares").getBoundingClientRect().left;
+}
+
+function getOffsetTop(elem){
+    return elem.offsetTop;
 }
 
 function outTable(){
     outCell();
     outRow();
+}
+
+function startPlacing(){
+    rightButton().style.marginTop = getOffsetTop((getSquares().children[0])) + "px";
+    bottomButton().style.marginLeft = getOffsetLeft((getSquares().children[0].children[0])) + "px";
 }
 
 window.onload = function() {
@@ -140,4 +139,6 @@ window.onload = function() {
     addColumn();
     addRow();
     addColumn();
+    startPlacing();
+    outTable();
 };
